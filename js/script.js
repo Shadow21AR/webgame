60709256c08c97a1f79bad273306120bc2ea4c37
Shadow <66721908+Shadow21AR@users.noreply.github.com>
2023-04-15 13:46:40 +0530

    touch?? :0

--- js/script.js
+++ js/script.js
@@ -1,57 +1,166 @@
+// Get player
 const player = document.querySelector("#player");
 const playerRect = player.getBoundingClientRect();
-const startButton = document.querySelector("#start-game");
-const game = document.querySelector("#game");
 const trail = document.querySelector(".trail");
+
+// Get div
+const game = document.querySelector("#game");
 const gameRect = game.getBoundingClientRect();
 
-player.style.visibility = "hidden";
+const controlsDiv = document.getElementById("controls");
 
-let gameState = "stopped";
+// Get button
+const startButton = document.querySelector("#start-button");
+const settingsButton = document.querySelector("#settings");
+
+//vars?
 let position = { x: 0, y: 0 };
 let speed = 5;
 let startTime = null;
 
+let gameState, playerFill;
+
+//init
+init();
+function init() {
+  // Initialize game state
+  gameState = false;
+  player.style.opacity = "0";
+
+  playerFill = localStorage.getItem('playerFill') || window.getComputedStyle(player).getPropertyValue('fill');
+  player.style.fill = playerFill;
+}
+
+// Add an event listener to the document for startbutton
 startButton.addEventListener("click", () => {
-  gameState = "running";
-  player.style.visibility = "visible";
-  startButton.style.visibility = "hidden";
-  game.style.display = "flex";
-  player.style.top = playerRect.top + "px";
-  player.style.left = playerRect.left + "px";
+  gameState = true;
+  player.style.opacity = 1;
+
+  const controlButtons = controlsDiv.querySelectorAll("button");
+  controlButtons.forEach((button) => {
+    button.style.display = "none";
+  });
+});
+
+// Add click event listener to the settings button
+settingsButton.addEventListener("click", () => {
+  startButton.remove();
+  settingsButton.remove();
+
+  // Create new control buttons
+  const playerColorButton = document.createElement("button");
+  playerColorButton.innerText = `Player Color`;
+  playerColorButton.style.color = playerFill;
+  controlsDiv.appendChild(playerColorButton);
+
+  const backButton = document.createElement("button");
+  backButton.innerText = "Back";
+  backButton.style.color = 'white';
+  controlsDiv.appendChild(backButton);
+
+  // Button listeners
+  backButton.addEventListener("click", () => {
+    // Remove the control buttons and back button
+    const controlButtons = controlsDiv.querySelectorAll("button");
+    controlButtons.forEach((button) => {
+      button.style.display = "none";
+    });
+
+    controlsDiv.appendChild(startButton);
+    controlsDiv.appendChild(settingsButton);
+  });
+
+  playerColorButton.addEventListener("click", () => {
+    const colorPicker = document.createElement("input");
+    colorPicker.type = "color";
+    colorPicker.addEventListener("input", () => {
+      const newPlayerFill = colorPicker.value;
+      player.style.fill = newPlayerFill;
+      playerColorButton.style.color = newPlayerFill;
+      localStorage.setItem('playerFill', newPlayerFill)
+    });
+    colorPicker.click();
+  });
 });
 
 // Add an event listener to the document for keydown events
+const pressedKeys = new Set();
+
 document.addEventListener("keydown", (event) => {
-  if (gameState === "stopped") {
-    return
+  if (!gameState) {
+    return;
   }
-  if (startTime === null) startTime = Date.now();
-
-  let elapsedTime = Date.now() - startTime;
-  
-  speed = elapsedTime <= 500 ? 10 :
-          elapsedTime <= 1000 ? 20 :
-          elapsedTime <= 1500 ? 30 :
-          elapsedTime <= 2000 ? 40 : 50;
-
-  if (event.key === "ArrowUp") {
-    movePlayer(0, -speed);
-  } else if (event.key === "ArrowDown") {
-    movePlayer(0, speed);
-  } else if (event.key === "ArrowLeft") {
-    movePlayer(-speed, 0);
-  } else if (event.key === "ArrowRight") {
-    movePlayer(speed, 0);
-  }
+  pressedKeys.add(event.code);
+  handleMovement();
 });
+
+document.addEventListener("keyup", (event) => {
+  pressedKeys.delete(event.code);
+  handleMovement();
+});
+
+function handleMovement() {
+  let speed = 20;
+  let directionX = 0;
+  let directionY = 0;
+
+  if (pressedKeys.has("ArrowLeft") || pressedKeys.has("KeyA")) {
+    directionX = -1;
+  }
+  if (pressedKeys.has("ArrowRight") || pressedKeys.has("KeyD")) {
+    directionX = 1;
+  }
+  if (pressedKeys.has("ArrowUp") || pressedKeys.has("KeyW")) {
+    directionY = -1;
+  }
+  if (pressedKeys.has("ArrowDown") || pressedKeys.has("KeyS")) {
+    directionY = 1;
+  }
+
+  // normalize diagonal movement speed
+  if (directionX !== 0 && directionY !== 0) {
+    speed /= Math.sqrt(2);
+  }
+
+  movePlayer(speed * directionX, speed * directionY);
+}
+
+// document.addEventListener("keydown", (event) => {
+//   if (!gameState) {
+//     return
+//   }
+//   const speed = 20;
+//   let directionX = 0;
+//   let directionY = 0;
+
+//   switch (event.code) {
+//     case "ArrowLeft":
+//       directionX = -1;
+//       break;
+//     case "ArrowRight":
+//       directionX = 1;
+//       break;
+//     case "ArrowUp":
+//       directionY = -1;
+//       break;
+//     case "ArrowDown":
+//       directionY = 1;
+//       break;
+//     default:
+//       return; // do nothing if a non-arrow key is pressed
+//   }
+
+//   if (directionX !== 0 && directionY !== 0) {
+//     directionX = directionX * Math.sqrt(2) / 2;
+//     directionY = directionY * Math.sqrt(2) / 2;
+//   }
+
+//   movePlayer(speed * directionX, speed * directionY);
+// });
 
 
 // Add an event listener to the document for keyup events
 document.addEventListener("keyup", (event) => {
-  if (gameState === "stopped") {
-    return
-  }
   speed = 5;
   startTime = null;
 });
@@ -79,6 +188,7 @@ function movePlayer(dx, dy) {
     dot.style.left = player.style.left;
     dot.style.height = player.style.height;
     dot.style.width = player.style.width;
+    dot.style.backgroundColor = player.style.fill;
     dot.addEventListener("transitionend", () => {
       trail.removeChild(dot);
     });
@@ -89,3 +199,80 @@ function movePlayer(dx, dy) {
   }
 }
 
+//touch support??
+const joystickContainer = document.getElementById('joystick-container');
+let joystick = null;
+
+const maxDistance = 40; // maximum distance the stick can move
+let stick;
+
+// add touchstart event listener
+document.addEventListener('touchstart', (event) => {
+  if (!gameState) {
+    return
+  }
+  const touch = event.touches[0];
+  const x = touch.clientX;
+  const y = touch.clientY;
+
+  // Create joystick
+  joystick = document.createElement('div');
+  joystick.classList.add('joystick');
+  joystick.style.left = x + 'px';
+  joystick.style.top = y + 'px';
+
+  // Create stick
+  stick = document.createElement('div');
+  stick.classList.add('stick');
+
+  // Add stick to joystick
+  joystick.appendChild(stick);
+
+  // Add joystick to container
+  joystickContainer.appendChild(joystick);
+});
+
+// add touchmove event listener
+document.addEventListener('touchmove', (event) => {
+  if (joystick) {
+    moveStick(event);
+  }
+});
+
+// add touchend event listener
+document.addEventListener('touchend', () => {
+  if (joystick) {
+    joystick.remove();
+    joystick = null;
+  }
+});
+
+// function to move the stick based on touch position
+function moveStick(event) {
+  const rect = joystick.getBoundingClientRect();
+  const touchX = event.touches[0].clientX - rect.left;
+  const touchY = event.touches[0].clientY - rect.top;
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+  const distance = Math.sqrt((touchX - centerX) ** 2 + (touchY - centerY) ** 2);
+  let stickX, stickY;
+
+  // limit the distance to the maximum distance
+  if (distance > maxDistance) {
+    const angle = Math.atan2(touchY - centerY, touchX - centerX);
+    stickX = Math.cos(angle) * maxDistance;
+    stickY = Math.sin(angle) * maxDistance;
+  } else {
+    stickX = touchX - centerX;
+    stickY = touchY - centerY;
+  }
+
+  // move the stick
+  stick.style.transform = `translate(${stickX}px, ${stickY}px)`;
+
+  // calculate the direction and move the player
+  const speed = 10;
+  const directionX = stickX / maxDistance;
+  const directionY = stickY / maxDistance;
+  movePlayer(speed * directionX, speed * directionY);
+}
